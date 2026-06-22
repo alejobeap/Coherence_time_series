95c9f15e484c75cbbc360f5df1ea6a5561d5c1a9
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2026-06-22 10:12:55 +0100

    Add Run_filter_longer_standar.sh script

---
+++ Run_filter_longer_standar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Estimated the average coherence"
+Estimate_Coherence_Average_from_DEM.py
+plot_histogram_average_coherence.py
+filtered_average.sh
+matriz_coherencia.py
+Largas_combinaciones_filtradas.sh
+echo "Standar conecciones"
+crear_standar_list.sh
